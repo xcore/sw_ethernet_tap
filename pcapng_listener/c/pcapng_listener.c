2e21d23d4afcf0ebd900275b21eac74f54b5287d
Peter Hedinger <peter@xmos.com>
2013-09-27 11:33:01 +0100

    Now working on Windows. Includes a couple of critical fixes (like number of bytes to read and the string parsing).

--- pcapng_listener/c/pcapng_listener.c
+++ pcapng_listener/c/pcapng_listener.c
@@ -7,17 +7,26 @@
  *  ./pcapng_listener 127.0.0.1 12346
  *
  */
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#ifdef _WIN32
+  #include <winsock.h>
+  #pragma comment(lib, "Ws2_32.lib")
+
+  // Provided by the inet_pton.c implementation locally
+  int inet_pton(int af, const char *src, void *dst);
+#else
+  #include <sys/socket.h>
+  #include <sys/types.h>
+  #include <netinet/in.h>
+  #include <netdb.h>
+  #include <unistd.h>
+  #include <errno.h>
+  #include <arpa/inet.h>
+#endif
+
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h>
 #include <assert.h>
 
 #include "pcapng.h"
@@ -58,7 +67,7 @@ void emit_section_header_block(FILE *f)
 
 void emit_interface_description_block(FILE *f)
 {
-  interface_description_block_t interface = {
+  interface_description_block_t iface = {
     0x1,                                    // Block Type
     sizeof(interface_description_block_t),  // Block Total Length
     0x1,                                    // LinkType
@@ -69,7 +78,7 @@ void emit_interface_description_block(FILE *f)
 
     sizeof(interface_description_block_t)   // Block Total Length
   };
-  fwrite(&interface, sizeof(interface), 1, f);
+  fwrite(&iface, sizeof(iface), 1, f);
 }
 
 FILE *g_log = NULL;
@@ -85,73 +94,18 @@ void int_handler(int sig)
   exit(1);
 }
 
-int main(int argc, char *argv[])
+void handle_socket(int sockfd)
 {
-  int sockfd = 0;
-  int n = 0;
-  unsigned char recv_buffer[MAX_RECV_BYTES];
-  struct sockaddr_in serv_addr;
-
-  if (DEBUG)
-    g_log = fopen("run.log", "w");
-
-  g_pcap_fptr = fopen("cap.pcapng", "wb");
-
-  signal(SIGINT, int_handler);
-
-  if (argc != 3) {
-    printf("Usage: %s <ip of server> <port>\n", argv[0]);
-    exit(1);
-  }
-
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("ERROR: Could not create socket\n");
-    exit(1);
-  }
-
-  memset(&serv_addr, 0, sizeof(serv_addr));
-
-  // Parse the port parameter
-  char *end_pointer = argv[2];
-  int port = strtol(argv[2], &end_pointer, 10);
-  if (end_pointer == argv[2]) {
-    printf("ERROR: Failed to parse port '%s'\n", argv[2]);
-    exit(1);
-  }
-
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(port);
-
-  if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
-    printf("ERROR: inet_pton error occured\n");
-    exit(1);
-  }
-
-  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    printf("ERROR: Connect failed\n");
-    exit(1);
-  }
-
-  emit_section_header_block(g_pcap_fptr);
-
-  // Emit two interface descriptions as there are two on the tap
-  emit_interface_description_block(g_pcap_fptr);
-  emit_interface_description_block(g_pcap_fptr);
-
-  // Send the command to request which event types to receive
-  recv_buffer[0] = EVENT_DATA | EVENT_PRINT;
-  n = send(sockfd, recv_buffer, 1, 0);
-  if (n != 1) {
-    printf("ERROR: Command send failed\n");
-    exit(1);
-  }
-
-  printf("Connected\n");
-
   int total_bytes = 0;
   int num_remaining_bytes = 0;
+  unsigned char recv_buffer[MAX_RECV_BYTES];
+  int n = 0;
 
-  while ((n = read(sockfd, &recv_buffer[num_remaining_bytes], sizeof(recv_buffer - num_remaining_bytes))) > 0) {
+#ifdef _WIN32
+  while ((n = recv(sockfd, &recv_buffer[num_remaining_bytes], sizeof(recv_buffer) - num_remaining_bytes, MSG_PARTIAL)) > 0) {
+#else
+  while ((n = read(sockfd, &recv_buffer[num_remaining_bytes], sizeof(recv_buffer) - num_remaining_bytes)) > 0) {
+#endif
     int i;
 
     if (DEBUG)
@@ -172,17 +126,20 @@ int main(int argc, char *argv[])
       // Indicate when a block of data has been handled by the fact that the pointer can move on
       int increment = 0;
 
-      if (recv_buffer[0] == EVENT_PRINT) {
+      if (recv_buffer[i] == EVENT_PRINT) {
         unsigned int string_len = 0;
 
         // Need one byte for type, then 8 bytes of time stamp and 4 bytes of length
         if ((i + PRINT_EVENT_BYTES) <= n) {
-          unsigned int string_len = EXTRACT_UINT(recv_buffer, 9);
+          unsigned int string_len = EXTRACT_UINT(recv_buffer, i + 9);
 
           // Ensure the buffer won't overflow
-          assert(string_len < MAX_RECV_BYTES);
           int string_start = i + PRINT_EVENT_BYTES;
           int string_end = i + PRINT_EVENT_BYTES + string_len;
+
+          // Assertion has to be after variable declaration for Windows c89 compile
+          assert(string_len < MAX_RECV_BYTES);
+
           if (string_end <= n) {
             // Ensure the string is null-terminated
             unsigned char tmp = recv_buffer[string_end];
@@ -200,14 +157,15 @@ int main(int argc, char *argv[])
 
       } else if (recv_buffer[0] == EVENT_DATA) {
         if ((i + DATA_EVENT_HEADER_BYTES) <= n) {
-          unsigned int packet_len = EXTRACT_UINT(recv_buffer, 4);
+          int packet_len = EXTRACT_UINT(recv_buffer, 4);
 
           if ((i + packet_len + DATA_EVENT_BYTES) <= n) {
+            // Data starts after the message header
+            int data_start = i + DATA_EVENT_HEADER_BYTES;
+
             // An entire packet has been received - write it to the file
             total_bytes += packet_len;
 
-            // Data starts after the message header
-            int data_start = i + DATA_EVENT_HEADER_BYTES;
             fwrite(&recv_buffer[data_start], packet_len, 1, g_pcap_fptr);
             increment = packet_len + DATA_EVENT_BYTES;
           }
@@ -233,6 +191,95 @@ int main(int argc, char *argv[])
       }
     }
   }
+}
+
+int main(int argc, char *argv[])
+{
+  int sockfd = 0;
+  int n = 0;
+  unsigned char command_buffer[1];
+  struct sockaddr_in serv_addr;
+  char *end_pointer = NULL;
+  int port = 0;
+
+  if (DEBUG)
+    g_log = fopen("run.log", "w");
+
+  g_pcap_fptr = fopen("cap.pcapng", "wb");
+
+  signal(SIGINT, int_handler);
+
+  if (argc != 3) {
+    printf("Usage: %s <ip of server> <port>\n", argv[0]);
+    exit(1);
+  }
+
+#ifdef _WIN32
+  {
+    //Start up Winsock
+    WSADATA wsadata;
+
+    int error = WSAStartup(0x0202, &wsadata);
+    if (error) {
+      printf("ERROR: WSAStartup failed with '%d'\n", error);
+      exit(1);
+    }
+
+    //Did we get the right Winsock version?
+    if (wsadata.wVersion != 0x0202) {
+      printf("ERROR: WSAStartup version incorrect '%x'\n", wsadata.wVersion);
+      WSACleanup();
+      exit(1);
+    }
+  }
+#endif // _WIN32
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("ERROR: Could not create socket\n");
+    exit(1);
+  }
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+
+  // Parse the port parameter
+  end_pointer = (char*)argv[2];
+  port = strtol(argv[2], &end_pointer, 10);
+  if (end_pointer == argv[2]) {
+    printf("ERROR: Failed to parse port '%s'\n", argv[2]);
+    exit(1);
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+
+  if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+    printf("ERROR: inet_pton error occured\n");
+    exit(1);
+  }
+
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("ERROR: Connect failed\n");
+    exit(1);
+  }
+
+  emit_section_header_block(g_pcap_fptr);
+
+  // Emit two interface descriptions as there are two on the tap
+  emit_interface_description_block(g_pcap_fptr);
+  emit_interface_description_block(g_pcap_fptr);
+
+  // Send the command to request which event types to receive
+  command_buffer[0] = EVENT_DATA | EVENT_PRINT;
+  n = send(sockfd, command_buffer, 1, 0);
+  if (n != 1) {
+    printf("ERROR: Command send failed\n");
+    exit(1);
+  }
+
+  printf("Connected\n");
+
+  handle_socket(sockfd);
 
   return 0;
 }
+
